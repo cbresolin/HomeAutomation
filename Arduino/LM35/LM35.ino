ad748ddb9ab6d1d2066f1c0ebb18eeaf8e89c3ec
CHRISTOPHE BRESOLIN <christophe.bresolin@gmail.com>
2017-04-15 21:39:00 +0100

    BT thermometer
    - read temperature remotely
    - control RGB LED remotely

--- Arduino/LM35/LM35.ino
+++ Arduino/LM35/LM35.ino
@@ -5,87 +5,92 @@
 * every increase 1 ℃, the output voltage increase 10 mv
 *****************************************/
 /****************************************/
-#include <Wire.h>
-#include <SoftwareSerial.h>
 
+// Libraries
+#include <Wire.h>
+#include <aREST.h>
+
+// Defines
 #define ML35PIN A0 
 #define REDPIN 4
 #define GREENPIN 5
 #define BLUEPIN 6
-#define COLD 18
-#define HOT 22
-
 #define SERIALBAUDRATE 9600
-#define BTRX 2
-#define BTTX 3
+#define DEVICENAME "BT Thermometer"
+#define DEVICEID "01"
 
-SoftwareSerial btserial(BTRX, BTTX);
+// Create aREST instance
+aREST rest = aREST();
 
-float temp = 0;
-float lmVal = 0;
+// Variables to be exposed to the API
+float temperature;
 
 void setup()
 {
   // initialize serial communication
   Serial.begin(SERIALBAUDRATE);
-  
-  // initialize BT serial communication
-  btserial.begin(SERIALBAUDRATE);
+
+  // Expose variables to REST API
+  rest.variable("temperature",&temperature);
+
+  // Expose functions to REST API
+  rest.function("red",set_led_red);
+  rest.function("green",set_led_green);
+  rest.function("blue",set_led_blue);
+  rest.function("off",switchoff_led);
+
+  // Give name and ID to device
+  rest.set_id(DEVICEID);
+  rest.set_name(DEVICENAME);
 
   // Set RGB LED pin modes
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
 
-  // improve measure precision by reducing measure range to 1.1V instead of 5V
+  // improve temperature measure precision
+  // by reducing measure range to 1.1V instead of 5V
   analogReference(INTERNAL);
 
   switchoff_led();
 }
 
-void set_led_red()
+int set_led_red()
 {
   analogWrite(REDPIN, 255);
   analogWrite(BLUEPIN, 0);
   analogWrite(GREENPIN, 0);
+  return 0;
 }
 
-void set_led_blue()
+int set_led_blue()
 {
   analogWrite(REDPIN, 0);
   analogWrite(BLUEPIN, 255);
   analogWrite(GREENPIN, 0);
+  return 0;
 }
 
-void set_led_green()
+int set_led_green()
 {
   analogWrite(REDPIN, 0);
   analogWrite(BLUEPIN, 0);
   analogWrite(GREENPIN, 255);
+  return 0;
 }
 
-void switchoff_led()
+int switchoff_led()
 {
   analogWrite(REDPIN, 0);
   analogWrite(BLUEPIN, 0);
   analogWrite(GREENPIN, 0);
+  return 0;
 }
 
 void loop()
 {
-  lmVal = analogRead(ML35PIN);
-  temp = (lmVal * (1.1 / 1023.0) * 100.0) - 1;
-  
-  btserial.print("Temperature: ");
-  btserial.print(temp);
-  btserial.println(" C");
+  temperature = (analogRead(ML35PIN) * (1.1 / 1023.0) * 100.0) - 1;
 
-  if (temp < COLD)
-      set_led_blue();
-  else if (temp >= COLD && temp < HOT)
-      set_led_green();
-  else if (temp >= HOT)
-      set_led_red();
-  
-  delay(100);
+  // Handle REST calls
+  rest.handle(Serial);
 }
