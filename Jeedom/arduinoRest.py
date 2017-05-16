e0997cec55de4740cc5927a67a75422b0ed08359
Christophe Brésolin <christophe.bresolin@gmail.com>
2017-05-16 09:29:43 +0200

    Handle case when JSON is malformed
    Signed-off-by: Christophe Brésolin <christophe.bresolin@gmail.com>

--- Jeedom/arduinoRest.py
+++ Jeedom/arduinoRest.py
@@ -3,6 +3,13 @@ import serial
 import argparse
 import sys
 
+def is_json(myjson):
+  try:
+    json_object = json.loads(myjson)
+  except ValueError, e:
+    return False
+  return True
+
 # Manage argument parser
 parser = argparse.ArgumentParser(description='Control Arduino Uno actions')
 parser.add_argument('-command', required=True, type=str, help='ReST command to be sent to Arduino (e.g. /id/, /blue/)')
@@ -16,12 +23,17 @@ try:
     serialPort.close()
 
     dictResp = json.loads(strResp)
-    print dictResp.get('temperature', 'None')
+    if "/temperature/" in args.command:
+        print dictResp.get('temperature', 'None')
+    else:
+        print dictResp.get('return_value', -1)
 
 except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
 except KeyError as e:
-    print "KeyError on {0}".format(e)
+    print "Key Error on {0}".format(e)
+except ValueError as e:
+    print "{0}!".format(e)
 except:
     print "Unexpected error:", sys.exc_info()[0]
     raise
