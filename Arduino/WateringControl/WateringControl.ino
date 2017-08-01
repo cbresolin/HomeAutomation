90b20af5db5a9e5266ef42df4e1b19ddd7f71e98
CHRISTOPHE BRESOLIN <christophe.bresolin@gmail.com>
2017-08-01 11:02:10 +0200

    Set pin modes
    - enable queries on those pins

--- Arduino/WateringControl/WateringControl.ino
+++ Arduino/WateringControl/WateringControl.ino
@@ -14,6 +14,18 @@
 #define DEVICENAME "Watering Control"
 #define DEVICEID "02"
 
+#define VBUSLANE1 22
+#define VBUSLANE2 28
+#define VBUSLANE3 29
+
+#define POWERLANE1 40
+#define POWERLANE2 38
+#define POWERLANE3 50
+
+#define SWITCHLANE1 36
+#define SWITCHLANE2 34
+#define SWITCHLANE3 52
+
 // Create aREST instance
 aREST rest = aREST();
 
@@ -22,11 +34,22 @@ void setup()
   // initialize serial communication
   Serial.begin(SERIALBAUDRATE);
 
-  // Expose functions to REST API
-
   // Give name and ID to device
   rest.set_id(DEVICEID);
   rest.set_name(DEVICENAME);
+
+  // Set pin modes
+  pinMode(VBUSLANE1, OUTPUT);
+  pinMode(VBUSLANE2, OUTPUT);
+  pinMode(VBUSLANE3, OUTPUT);
+
+  pinMode(POWERLANE1, OUTPUT);
+  pinMode(POWERLANE2, OUTPUT);
+  pinMode(POWERLANE3, OUTPUT);
+
+  pinMode(SWITCHLANE1, OUTPUT);
+  pinMode(SWITCHLANE2, OUTPUT);
+  pinMode(SWITCHLANE3, OUTPUT);
 }
 
 void loop()
