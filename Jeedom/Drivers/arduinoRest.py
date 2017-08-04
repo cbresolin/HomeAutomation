5f5819f18e4b38f5a44ee9df32038993f8f2373d
Christophe Brésolin <christophe.bresolin@gmail.com>
2017-08-04 12:20:54 +0200

    Fix usage of readline()

--- Jeedom/Drivers/arduinoRest.py
+++ Jeedom/Drivers/arduinoRest.py
@@ -2,7 +2,7 @@ import json
 import serial
 import argparse
 import sys
-import time
+import io
 
 # Manage argument parser
 parser = argparse.ArgumentParser(description='Control Arduino Uno actions')
@@ -12,13 +12,14 @@ parser.add_argument('-device', type=str, default='/dev/rfcomm0', help='Device to
 args = parser.parse_args()
 
 try:
-    serialPort = serial.Serial(port=args.device, timeout=30)
-    time.sleep(1)
-    serialPort.write(args.command)
-    strResp = serialPort.readline()
+    serialPort = serial.Serial(port=args.device, timeout=5)
+    sio = io.TextIOWrapper(io.BufferedRWPair(serialPort, serialPort))
+    sio.write(unicode(args.command))
+    sio.flush()
+    UniResp = sio.readline()
     serialPort.close()
 
-    dictResp = json.loads(strResp)
+    dictResp = json.loads(UniResp)
 
     # Return values according to content
     if "message" in dictResp:
@@ -29,11 +30,11 @@ try:
         print dictResp
 
 except IOError as e:
-    print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    print "I/O error ({0}):{1}".format(e.errno, e.strerror)
 except KeyError as e:
     print "Key Error on {0}".format(e)
 except ValueError as e:
-    print "{0}!".format(e)
+    print "Value error ({0})".format(e)
 except:
     print "Unexpected error:", sys.exc_info()[0]
     raise
